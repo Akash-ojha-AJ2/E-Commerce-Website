79581ba7e1b4687ecb5c498f62ceaf8d6adcd3ee
Akash-ojha-AJ2 <jhak2594@gmail.com>
2025-10-25 07:57:48 -0700

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,25 +5,18 @@ import nodemailer from "nodemailer";
 export const sendEmail = async ({ email, subject, html }) => {
   try {
     // Create transporter
-    // const transporter = nodemailer.createTransport({
-    //   host: process.env.SMTP_HOST,
-    //   service: process.env.SMTP_SERVICE, // optional if host is provided
-    //   port: process.env.SMTP_PORT || 587,
-    //   secure: false, // true for 465, false for other ports
-    //   auth: {
-    //     user: process.env.SMTP_MAIL,
-    //     pass: process.env.SMTP_PASSWORD,
-    //   },
-    // });
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      service: process.env.SMTP_SERVICE, // optional if host is provided
+      port: process.env.SMTP_PORT || 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
 
-    // Create transporter
-    const transporter = nodemailer.createTransport({
-      service: process.env.SMTP_SERVICE, // 'gmail'
-      auth: {
-        user: process.env.SMTP_MAIL,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
+    
 
     // Mail options
     const mailOptions = {

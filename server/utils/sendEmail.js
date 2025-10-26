1f802bd4460b8a37b594ff1c19d462b32befa0b9
Akash-ojha-AJ2 <jhak2594@gmail.com>
2025-10-25 22:21:48 -0700

    Refactor sendEmail function to use environment variables

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -40,35 +40,66 @@
 
 
 
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-export const sendEmail = async ({ email, subject, html }) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      host: 'smtp.gmail.com',
-      port: 465, // ✅ 465 try karo (587 block hai)
-      secure: true, // ✅ 465 ke liye true
-      auth: {
-        user: process.env.SMTP_MAIL,
-        pass: process.env.SMTP_PASSWORD, // Gmail App Password
-      },
-      connectionTimeout: 60000, // 60 seconds
-      greetingTimeout: 60000
-    });
+// export const sendEmail = async ({ email, subject, html }) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: 'gmail',
+//       host: 'smtp.gmail.com',
+//       port: 465, // ✅ 465 try karo (587 block hai)
+//       secure: true, // ✅ 465 ke liye true
+//       auth: {
+//         user: process.env.SMTP_MAIL,
+//         pass: process.env.SMTP_PASSWORD, // Gmail App Password
+//       },
+//       connectionTimeout: 60000, // 60 seconds
+//       greetingTimeout: 60000
+//     });
 
-    const mailOptions = {
-      from: `"ShopKart" <${process.env.SMTP_MAIL}>`,
-      to: email,
-      subject,
-      html,
-    };
+//     const mailOptions = {
+//       from: `"ShopKart" <${process.env.SMTP_MAIL}>`,
+//       to: email,
+//       subject,
+//       html,
+//     };
 
-    const info = await transporter.sendMail(mailOptions);
-    console.log("✅ Email sent:", info.messageId);
-    return info;
-  } catch (error) {
-    console.error("❌ Email sending failed:", error.message);
-    throw error;
-  }
+//     const info = await transporter.sendMail(mailOptions);
+//     console.log("✅ Email sent:", info.messageId);
+//     return info;
+//   } catch (error) {
+//     console.error("❌ Email sending failed:", error.message);
+//     throw error;
+//   }
+// };
+
+
+
+
+
+
+
+
+
+
+import nodeMailer from "nodemailer";
+
+export const sendEmail = async ({ email, subject, message }) => {
+  const transporter = nodeMailer.createTransport({
+    host: process.env.SMTP_HOST,
+    service: process.env.SMTP_SERVICE,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const options = {
+    from: `"ShopKart" <${process.env.SMTP_MAIL}>`,
+    to: email,
+    subject,
+    html,
+  };
+  await transporter.sendMail(options);
 };
